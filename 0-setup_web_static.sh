cf29bcc9ca7adadc25e3ef6c0528af98cc35c222
Obi <williamobi818@gmail.com>
2023-10-05 19:39:14 +0000

    push bck

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,9 +13,9 @@ sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
 echo "Holberton School" | sudo tee "/data/web_static/releases/test/index.html"
 
-sudo In sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -hR ubuntu: ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\n' /etc/nginx/sites-available/default
 
