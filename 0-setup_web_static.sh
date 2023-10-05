8d69095a73924d3cc8753e0ece617bdc3c6ebfff
Obi <williamobi818@gmail.com>
2023-10-05 12:46:03 +0000

    j

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#  a Bash script that sets up your web servers for the deployment of web_static
+# a Bash script that sets up your web servers for the deployment of web_static
 
 # Install Nginx if it not already installed
 if ! dpkg -l | grep -q "nginx"; then
