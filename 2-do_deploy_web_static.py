092fd13fbc3ab37f5ea1c7ba5ae9ea69956c2c80
Obi <williamobi818@gmail.com>
2023-10-08 10:00:50 +0000

    trial 01 sunday

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,78 +1,67 @@
 #!/usr/bin/python3
-""" This module handles packing up and distribution"""
+"""This script handles packing up and distributing the web_static code"""
 
 import os
-from fabric.api import local, put, env, run, task
+from fabric import Connection, task
 from datetime import datetime
 import tarfile
 
-from fabric.decorators import runs_once
-
-env.hosts = ['52.86.222.148', '3.85.41.223']
-
+# Define the remote hosts
+env_hosts = ['52.86.222.148', '3.85.41.223']
 
 @task
-@runs_once
 def do_pack():
-    """ method handles the packing up file"""
-    name = (f"web_static_{datetime.now().year}{datetime.now().month}"
-            f"{datetime.now().day}{datetime.now().hour}{datetime.now().minute}"
-            f"{datetime.now().second}")
-    M_name = f"{name}.tgz"
-    F_name = f"versions/{M_name}"
+    """Create a compressed archive of the web_static folder"""
+    now = datetime.now()
+    timestamp = now.strftime('%Y%m%d%H%M%S')
+    archive_name = f"web_static_{timestamp}.tgz"
 
     if not os.path.exists("versions"):
         os.makedirs("versions")
 
-    with tarfile.open(F_name, 'w:gz') as archive:
-        archive.add('web_static')
+    with tarfile.open(f"versions/{archive_name}", 'w:gz') as archive:
+        archive.add('web_static', arcname=os.path.basename('web_static'))
 
-    if os.path.exists(F_name):
-        return F_name
+    archive_path = os.path.join(os.getcwd(), "versions", archive_name)
+    if os.path.exists(archive_path):
+        return archive_path
     else:
         return None
 
-
 @task
 def do_deploy(archive_path):
-    """A method that distributes an archive to the web servers"""
+    """Distribute the archive to the web servers and set up the deployment"""
     if not os.path.exists(archive_path):
+        print("Archive not found.")
         return False
 
     try:
         # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, '/tmp/')
-        # Extract the filename without extension
-        file_name = os.path.basename(archive_path)
-        name, ext = os.path.splitext(file_name)
+        with Connection(env_hosts[0]) as conn:
+            conn.put(archive_path, '/tmp/')
 
-        # Uncompress the archive to the folder
-        # /data/web_static/releases/<archive filename without extension> on
-        # the web server
-        run(f'rm -f {archive_path}')
-        run(f'mkdir -p /data/web_static/releases/{name}/')
-        run(f'tar -xzf /tmp/{file_name} -C /data/web_static/releases/{name}/')
+            # Extract the filename without extension
+            file_name = os.path.basename(archive_path)
+            name, _ = os.path.splitext(file_name)
 
-        # Delete the archive from the web server
-        run(f'rm /tmp/{file_name}')
+            # Uncompress the archive to the folder
+            conn.run(f'mkdir -p /data/web_static/releases/{name}/')
+            conn.run(f'tar -xzf /tmp/{file_name} -C /data/web_static/releases/{name}/')
 
-        run(f'mv /data/web_static/releases/{name}'
-            f'/web_static/* '
-            f'/data/web_static/releases/{name}/')
+            # Delete the archive from the web server
+            conn.run(f'rm /tmp/{file_name}')
 
-        run(f'rm -rf /data/web_static/releases/{name}/web_static')
+            conn.run(f'mv /data/web_static/releases/{name}/web_static/* /data/web_static/releases/{name}/')
+            conn.run(f'rm -rf /data/web_static/releases/{name}/web_static')
 
-        # Delete the symbolic link /data/web_static/current from the web
-        # server
-        run('rm -rf /data/web_static/current')
+            # Delete the symbolic link /data/web_static/current from the web server
+            conn.run('rm -rf /data/web_static/current')
 
-        # Create a new the symbolic link /data/web_static/current on the
-        # web server, linked to the new version of your code (
-        # /data/web_static/releases/<archive filename without extension>)
-        run(f'ln -s /data/web_static/releases/{name}/ /data/web_static'
-            f'/current')
+            # Create a new symbolic link
+            conn.run(f'ln -s /data/web_static/releases/{name}/ /data/web_static/current')
+
         print('New version deployed!')
         return True
     except Exception as e:
-        june = str(e)
+        print(f"An error occurred: {str(e)}")
         return False
