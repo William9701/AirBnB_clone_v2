bdbe228044e810f8929fd2ba0e0a66e0516d9b7e
Obi <williamobi818@gmail.com>
2023-10-08 10:12:40 +0000

    sun

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This script handles packing up and distributing the web_static code"""
+""" This script handles packing up and distributing the web_static code"""
 
 import os
 from fabric import Connection, task
@@ -10,25 +10,6 @@ import tarfile
 env_hosts = ['52.86.222.148', '3.85.41.223']
 
 @task
-def do_pack():
-    """Create a compressed archive of the web_static folder"""
-    now = datetime.now()
-    timestamp = now.strftime('%Y%m%d%H%M%S')
-    archive_name = f"web_static_{timestamp}.tgz"
-
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
-
-    with tarfile.open(f"versions/{archive_name}", 'w:gz') as archive:
-        archive.add('web_static', arcname=os.path.basename('web_static'))
-
-    archive_path = os.path.join(os.getcwd(), "versions", archive_name)
-    if os.path.exists(archive_path):
-        return archive_path
-    else:
-        return None
-
-@task
 def do_deploy(archive_path):
     """Distribute the archive to the web servers and set up the deployment"""
     if not os.path.exists(archive_path):
@@ -36,32 +17,34 @@ def do_deploy(archive_path):
         return False
 
     try:
-        # Upload the archive to the /tmp/ directory of the web server
-        with Connection(env_hosts[0]) as conn:
-            conn.put(archive_path, '/tmp/')
+        # Extract the filename without extension
+        file_name = os.path.basename(archive_path)
+        name, _ = os.path.splitext(file_name)
 
-            # Extract the filename without extension
-            file_name = os.path.basename(archive_path)
-            name, _ = os.path.splitext(file_name)
+        for host in env_hosts:
+            # Upload the archive to the /tmp/ directory of each web server
+            with Connection(host) as conn:
+                conn.put(archive_path, '/tmp/')
 
-            # Uncompress the archive to the folder
-            conn.run(f'mkdir -p /data/web_static/releases/{name}/')
-            conn.run(f'tar -xzf /tmp/{file_name} -C /data/web_static/releases/{name}/')
+                # Uncompress the archive to the folder on each web server
+                conn.run(f'mkdir -p /data/web_static/releases/{name}/')
+                conn.run(f'tar -xzf /tmp/{file_name} -C /data/web_static/releases/{name}/')
 
-            # Delete the archive from the web server
-            conn.run(f'rm /tmp/{file_name}')
+                # Delete the archive from each web server
+                conn.run(f'rm /tmp/{file_name}')
 
-            conn.run(f'mv /data/web_static/releases/{name}/web_static/* /data/web_static/releases/{name}/')
-            conn.run(f'rm -rf /data/web_static/releases/{name}/web_static')
+                conn.run(f'mv /data/web_static/releases/{name}/web_static/* /data/web_static/releases/{name}/')
+                conn.run(f'rm -rf /data/web_static/releases/{name}/web_static')
 
-            # Delete the symbolic link /data/web_static/current from the web server
-            conn.run('rm -rf /data/web_static/current')
+                # Delete the symbolic link /data/web_static/current from each web server
+                conn.run('rm -rf /data/web_static/current')
 
-            # Create a new symbolic link
-            conn.run(f'ln -s /data/web_static/releases/{name}/ /data/web_static/current')
+                # Create a new symbolic link on each web server
+                conn.run(f'ln -s /data/web_static/releases/{name}/ /data/web_static/current')
 
-        print('New version deployed!')
+        print('New version deployed')
         return True
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-        return False
+        return False  
+
